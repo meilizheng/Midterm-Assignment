b08be719fe0a681cd9a6dc1154b34fecafdfecfc
meilizheng <116130097+meilizheng@users.noreply.github.com>
2023-02-07 22:35:12 -0800

    Added code
    Added Code

---
+++ Apartment.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Midterm_Assignment
+{
+    internal class Apartment
+    {
+        string ApartmentNumber;
+        string FirstName;
+        string LastName;
+        float numberOfBedrooms;
+        decimal MonthlyPayment;
+        decimal CurrentBalance;        
+        string ApartmentNotes;
+        bool isOccupied;
+
+        public Apartment(string apartmentNumber, string firstName, string lastName, float numberOfBedrooms, decimal monthlyPayment)
+        {
+            Random random = new Random();
+            ApartmentNumber = apartmentNumber;
+            FirstName = firstName;
+            LastName = lastName;
+            this.numberOfBedrooms = numberOfBedrooms;
+            MonthlyPayment = monthlyPayment;
+            this.isOccupied = true;
+        }
+
+        public Apartment(string apartmentNumber, bool IsOccupied)
+        {
+            Random random = new Random();
+            this.ApartmentNumber = apartmentNumber;
+            this.isOccupied = false;
+        }
+
+       
+
+        public string ApartmentNumber1 { get => ApartmentNumber; set => ApartmentNumber = value; }
+        public string FirstName1 { get => FirstName; set => FirstName = value; }
+        public string LastName1 { get => LastName; set => LastName = value; }
+        public float NumberOfBedrooms { get => numberOfBedrooms; set => numberOfBedrooms = value; }
+        public decimal MonthlyPayment1 { get => MonthlyPayment; set => MonthlyPayment = value; }
+        public decimal CurrentBalance1 { get => CurrentBalance; set => CurrentBalance = value; }
+        public string ApartmentNotes1 { get => ApartmentNotes; set => ApartmentNotes = value; }
+        public bool IsOccupied { get => isOccupied; set => isOccupied = value; }
+
+        public override string ToString()
+        {
+
+            if (isOccupied)
+            {
+                return $"Apartment#: {ApartmentNumber} -- First Name: {FirstName} -- Last Name: {LastName} -- Total of bedroom: {numberOfBedrooms} -- Monthly Payment: ${MonthlyPayment}";
+            }
+            else
+            {
+                return $"Apartment#: {ApartmentNumber} -- vacant";
+            }
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,58 @@ namespace Midterm_Assignment
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Apartment>apartments = new List<Apartment>();
         public MainWindow()
         {
             InitializeComponent();
+            Preload();
+            DisplayInformation();
         }
+        public void Preload()
+        {
+            Random rand = new Random();
+            for (int i = 100; i < 301; i++)
+            {
+                string apart = "H" + i;
+                if (rand.Next(2) != 0)
+                {
+                    decimal monthly = rand.Next(1500, 4500);
+                    float bedrooms = rand.Next(1, 4);
+                    //Apartment apartment = new Apartment(apart, "Alisa", "Bobik", bedrooms, monthly);
+                    //apartments.Add(apartment);
+                    //apartments.Add(new Apartment(apart, "Meili", "Zheng", bedrooms, monthly));
+                    //apartments.Add(new Apartment(apart, "Robert", "Kwan", bedrooms, monthly));
+                    //apartments.Add(new Apartment(apart, "Oliva", "Park", bedrooms, monthly));
+                    //apartments.Add(new Apartment(apart, "Sara", "Smith", bedrooms, monthly));
+                }
+                else
+                {
+                    apartments.Add(new Apartment(apart, false));
+                }
+            }
+        }
+
+        private void btnAddUpdateTenant_Click(object sender, RoutedEventArgs e)
+        {
+            string ApartNumber = txtAptNum.Text;
+            string FirstName = txtFirstName.Text;
+            string LastName = txtLastName.Text;
+            string MonthlyPay = txtMontylyPayments.Text;
+            string TotalBedRoom = txtTotalbedroom.Text;
+            string TennantInformation = $"Apart#: {ApartNumber} - First Name: {FirstName}- Last Name: {LastName} - Monthly Payment: {MonthlyPay} - Total Bedroom: {TotalBedRoom}";
+            lbDisplayTennantInfo.Items.Add(TennantInformation);            
+        }
+
+        public void DisplayInformation()
+        {
+            lbDisplayTennantInfo.Items.Clear();
+
+            for (int i = 0; i < apartments.Count; i++)
+            {
+                lbDisplayTennantInfo.Items.Add(apartments[i]);
+            }
+        }
+
+       
     }
 }
